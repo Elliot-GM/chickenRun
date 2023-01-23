ab47ca61d7d3ced77f117cdc80ed1806c40a5acc
Elliot GM <elliot.giron-michel@epitech.eu>
2023-01-23 04:27:59 +0100

    fix(main): express() => app in app.js

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ require("dotenv").config();
 const { connection } = require("./src/services/connection");
 const userRoutes = require("./src/routes/chicken");
 const express = require("express");
+const app = express();
 const port = process.env.PORT || 3000;
 
 connection().catch(error => console.log(error));
-express().use(express.json());
-express().use(userRoutes);
+app.use(express.json());
+app.use(userRoutes);
 
-express().listen(port, () => {
+app.listen(port, () => {
     console.log("Welcome to the chicken run !");
 })
