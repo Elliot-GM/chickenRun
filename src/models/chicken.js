e0f95001cabd8e7cfe2e01b2e70a5cfebaf01f0d
Elliot GM <elliot.giron-michel@epitech.eu>
2023-01-23 03:19:09 +0100

    refactor(main): chicken model with : name, birthday, weight, steps and isRunning

--- src/models/chicken.js
+++ src/models/chicken.js
@@ -4,6 +4,30 @@ const Chicken = mongoose.model("Chicken", {
     name: {
         type: String,
         required: true
+    },
+    birthday: {
+        type: Date,
+        required: false
+    },
+    weight: {
+        type: Number,
+        required: true,
+        validate(value) {
+            if (value < 0)
+                throw new Error('Weight must be positive');
+        }
+    },
+    steps: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0)
+                throw new Error('Steps must be positive');
+        }
+    },
+    isRunning: {
+        type: Boolean,
+        default: false,
     }
 });
 
